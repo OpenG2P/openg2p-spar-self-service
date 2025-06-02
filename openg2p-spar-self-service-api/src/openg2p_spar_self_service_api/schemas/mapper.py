dd057ce7c47d33b19f3d0ba8ecff021cfd59a483
tahzeer <tahzeer.ash@gmail.com>
2025-06-02 13:02:07 +0530

    pre-commit fixes

--- openg2p-spar-self-service-api/src/openg2p_spar_self_service_api/schemas/mapper.py
+++ openg2p-spar-self-service-api/src/openg2p_spar_self_service_api/schemas/mapper.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, List, Optional
 
 from pydantic import BaseModel
 
-from .dfsp import LevelTypeEnum, FaTypeEnum
+from .dfsp import FaTypeEnum
 from .request import SparRequest
 from .response import SparResponse
 
